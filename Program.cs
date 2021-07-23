a1758b616722e54d8ee7d08a526b8b4b86db2500
BootCampBL <satyabhandari43@gmail.com>
2021-07-23 23:44:02 +0530

    [sathya] Added - Checking Max of 3 Floats

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day13_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
+            Console.WriteLine(output);
+
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(12.7, 20.47, 80.56);
+            Console.WriteLine(doubleoutput);
+        }
+    }
+}

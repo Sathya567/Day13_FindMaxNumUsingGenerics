a7dad285009d505e7828fbab6af10e4dffa62fe1
BootCampBL <satyabhandari43@gmail.com>
2021-07-23 23:39:05 +0530

    [sathya] Added - Checking Max Of 3 Integers

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day13_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
+            Console.WriteLine(output);
+        }
+    }
+}
